c878208fd63ad61922270633f6de9a759ffa1040
naomieniola <naomiadesiyan@hotmail.com>
2025-02-14 17:12:56 +0000

    gen-ai

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import *
+from flask import Flask, render_template, session
+import google.generativeai as genai
 
-app = Flask("my-api")
+app = Flask("flaskwithGemini")
+app.secret_key = "abc"
 
-@app.route("/")
-def home():
+genai.configure(api_key="YOUR_GEMINI_API_KEY")
+
+prompt = """Write Recommendations to be followed by SRE when the production 
+respond with 3 suggestions and order by priority and less than 100 lines.
+Respond in html, ignore header and body tags"""
+
+@app.route('/')
+def Home():
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(prompt)
+    session["text"] = response.text.strip("\n").strip("").lstrip("html")
     return render_template('index.html')
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
