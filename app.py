17137529a98f0cdac0cb99f9e10d44f431ef4f4b
naomieniola <naomiadesiyan@hotmail.com>
2025-02-15 14:12:09 +0000

    added-flask-venv

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, session
+from flask import *
 import google.generativeai as genai
 
+"""
 app = Flask("flaskwithGemini")
 app.secret_key = "abc"
 
 genai.configure(api_key="YOUR_GEMINI_API_KEY")
 
-prompt = """Write Recommendations to be followed by SRE when the production 
+prompt = "Write Recommendations to be followed by SRE when the production 
 respond with 3 suggestions and order by priority and less than 100 lines.
-Respond in html, ignore header and body tags"""
+Respond in html, ignore header and body tags"
 
 @app.route('/')
 def Home():
@@ -17,5 +18,13 @@ def Home():
     session["text"] = response.text.strip("\n").strip("").lstrip("html")
     return render_template('index.html')
 
-if __name__ == "__main__":
+"""
+
+app = Flask ("gemini-api")
+
+@app.route("/")
+def Index():
+    return render_template('index.html')
+#passing data from route url to html page
+
     app.run(debug=True)
